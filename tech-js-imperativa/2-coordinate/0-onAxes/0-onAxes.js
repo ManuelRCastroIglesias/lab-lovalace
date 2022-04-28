d4066c968709643c5ac53aba8bf9f80fb8b5fe50
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-04-28 21:21:49 +0200

    Update 0-onAxes.js
    Corregidos errores varios.

--- tech-js-imperativa/2-coordinate/0-onAxes/0-onAxes.js
+++ tech-js-imperativa/2-coordinate/0-onAxes/0-onAxes.js
@@ -21,18 +21,19 @@ const console = new Console();
  * Dame la abscisa de la coordenada:  0
  * Dame la ordenada de la coordenada:  0
  * La coordenada (0, 2) está en el eje de abscisa y de ordenadas
- * 
- * coordinate TextQuadrant
- * 
  */
 
-const abscissa = console.readNumber("Dame las horas: ");
-const ordinate = console.readNumber("Dame las horas: ");
+const abscissa = console.readNumber("Dame la abscisa de la coordenada: ");
+const ordinate = console.readNumber("Dame la ordenada de la coordenada: ");
 
-const    textOnAxisX = abscissa === 0				? " está sobre el eje de abscisas"  : "" ;
-const    textOnAxisY = ordinate === 0				? " está sobre el eje de ordenadas" : "" ;
-const textOriginAxis = abscissa === 0 === ordinate  ? " están sobre los ejes de abscisas y de ordenadas"
-                     : abscissa !== 0 !== ordinate  ? " no están sobre ninguno de los ejes" : "" ;
-// const   textQuadrant = abscissa === 0 ;
+// const    textOnAxisX = abscissa === 0				? " está sobre el eje de abscisas"  : "" ;
+// const    textOnAxisY = ordinate === 0				? " está sobre el eje de ordenadas" : "" ;
+// const textOriginAxis = abscissa === 0 === ordinate  ? " están sobre los ejes de abscisas y de ordenadas"
+//                      : abscissa !== 0 !== ordinate  ? " no están sobre ninguno de los ejes" : "" ;
+let textQuadrant =
+abscissa === 0 && ordinate === 0 ? " están sobre los ejes de abscisas y de ordenadas" :
+abscissa !== 0 && ordinate !== 0 ?  "no está en ningún eje" :
+abscissa === 0 && ordinate !== 0 ?  "está en el eje de abscisa" :
+abscissa !== 0 && ordinate === 0 ?  "está en el eje de ordenadas" : "" ;
 
-console.writeln("La coordenada `(`${abscissa}, {ordinate}`)` {TextQuadrant}.")
+console.writeln("La coordenada (" + abscissa + "," + ordinate + ")" + textQuadrant+".")
