3b1346354e0f282e2944938bfa67428ef2f01e53
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-04-28 21:37:36 +0200

    Creado Archivo

---
+++ tech-js-imperativa/2-coordinate/1-quadrant/1 - quadrant.js
@@ -0,0 +1,17 @@
+/// 2 - coordinate / 1 - quadrant
+/// Dame la abcisa de la coordenada: 2
+/// Dame la ordenada de la coordenada: 2
+/// 
+/// La coordenada(2, 2) está en el 1º cuadrante.
+/// Dame la abcisa de la coordenada: -2
+/// Dame la ordenada de la coordenada: 2
+/// 
+/// La coordenada(-2, 2) está en el 2º cuadrante.
+/// Dame la abcisa de la coordenada: -2
+/// Dame la ordenada de la coordenada: -2
+/// 
+/// La coordenada(-2, -2) está en el 3º cuadrante.
+/// Dame la abcisa de la coordenada: 2
+/// Dame la ordenada de la coordenada: -2
+/// 
+/// La coordenada(2, -2) está en el 4º cuadrante.
