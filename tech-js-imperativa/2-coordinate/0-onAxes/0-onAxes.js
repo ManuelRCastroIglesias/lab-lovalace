619bce5ad8a10417670cc00533ccadabf569a32f
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-04-28 21:26:27 +0200

    Update 0-onAxes.js
    Quitados comentarios innecesarios.
    Cambio de varible let a const

--- tech-js-imperativa/2-coordinate/0-onAxes/0-onAxes.js
+++ tech-js-imperativa/2-coordinate/0-onAxes/0-onAxes.js
@@ -2,35 +2,11 @@ const { Console } = require("console-mpds");
 const console = new Console();
 // --------------------------------------------/
 /// <Autor:> Manuel Rosendo Castro Iglesias.
-/*
- *  # 2-coordinate
- * ## 0-onAxes
- * 
- * Dame la abscisa de la coordenada:  2
- * Dame la ordenada de la coordenada:  2
- * La coordenada (2, 2) no está en ningún eje
- * 
- * Dame la abscisa de la coordenada:  0
- * Dame la ordenada de la coordenada:  2
- * La coordenada (0, 2) está en el eje de abscisa
- * 
- * Dame la abscisa de la coordenada:  2
- * Dame la ordenada de la coordenada:  0
- * La coordenada (0, 2) está en el eje de ordenadas
- * 
- * Dame la abscisa de la coordenada:  0
- * Dame la ordenada de la coordenada:  0
- * La coordenada (0, 2) está en el eje de abscisa y de ordenadas
- */
 
 const abscissa = console.readNumber("Dame la abscisa de la coordenada: ");
 const ordinate = console.readNumber("Dame la ordenada de la coordenada: ");
 
-// const    textOnAxisX = abscissa === 0				? " está sobre el eje de abscisas"  : "" ;
-// const    textOnAxisY = ordinate === 0				? " está sobre el eje de ordenadas" : "" ;
-// const textOriginAxis = abscissa === 0 === ordinate  ? " están sobre los ejes de abscisas y de ordenadas"
-//                      : abscissa !== 0 !== ordinate  ? " no están sobre ninguno de los ejes" : "" ;
-let textQuadrant =
+const textQuadrant =
 abscissa === 0 && ordinate === 0 ? " están sobre los ejes de abscisas y de ordenadas" :
 abscissa !== 0 && ordinate !== 0 ?  "no está en ningún eje" :
 abscissa === 0 && ordinate !== 0 ?  "está en el eje de abscisa" :
