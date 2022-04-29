91fe6c6ba6aa5b6c13e456c15ef54ac4605c2b6d
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-04-29 15:44:31 +0200

    Terminado ejercicio quadrant. Se puede optimizar con valores booleanos.  1 = true  0 = undefined -1 = false

--- tech-js-imperativa/2-coordinate/1-quadrant/1 - quadrant.js
+++ tech-js-imperativa/2-coordinate/1-quadrant/1 - quadrant.js
@@ -2,22 +2,31 @@ const { Console } = require("console-mpds");
 const console = new Console();
 // --------------------------------------------/
 /// <Autor:> Manuel Rosendo Castro Iglesias.
-
-
 /// 2 - coordinate / 1 - quadrant
 /// Dame la abcisa de la coordenada: 2
+const abscissa = console.readNumber("Dame la abscisa de la coordenada: ");
 /// Dame la ordenada de la coordenada: 2
-/// 
-/// La coordenada(2, 2) está en el 1º cuadrante.
-/// Dame la abcisa de la coordenada: -2
-/// Dame la ordenada de la coordenada: 2
-/// 
-/// La coordenada(-2, 2) está en el 2º cuadrante.
-/// Dame la abcisa de la coordenada: -2
-/// Dame la ordenada de la coordenada: -2
-/// 
-/// La coordenada(-2, -2) está en el 3º cuadrante.
-/// Dame la abcisa de la coordenada: 2
-/// Dame la ordenada de la coordenada: -2
-/// 
-/// La coordenada(2, -2) está en el 4º cuadrante.
+const ordinate = console.readNumber("Dame la ordenada de la coordenada: ");
+console.writeln();
+
+const relativAbscissa = abscissa > 0 ? 1 : abscissa < 0 ? -1 : 0;
+const relativOrdinate = ordinate > 0 ? 1 : ordinate < 0 ? -1 : 0;
+
+const quadrant = relativAbscissa + relativAbscissa + relativOrdinate;
+
+const overQuadrant =
+	quadrant === 3 ? " está en el primer cuadrante" :
+		quadrant === -1 ? " está en el segundo cuadrante" :
+			quadrant === -3 ? " está en el tercer cuadrante" :
+				quadrant === 1 ? " está en el cuarto cuadrante" : false;
+
+// Se mantiene del ejercicio anterior. Por compatibilidad.
+const textOnAxis =
+	abscissa === 0 && ordinate === 0 ? " está sobre el origen de coordenadas" :
+		abscissa !== 0 && ordinate !== 0 ? overQuadrant :
+			abscissa === 0 && ordinate !== 0 ? " está sobre el eje de abscisa" :
+				abscissa !== 0 && ordinate === 0 ? " está sobre el eje de ordenadas" : "";
+
+console.writeln(`La coordenada (${abscissa}, ${ordinate})"${textOnAxis}.`)
+
+
