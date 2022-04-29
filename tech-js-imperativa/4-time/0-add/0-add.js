ec3a1f52fe1bcf436a46a6edcbee79921b9c6537
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-04-29 10:11:29 +0200

    Resuelto el ejercicio.

--- tech-js-imperativa/4-time/0-add/0-add.js
+++ tech-js-imperativa/4-time/0-add/0-add.js
@@ -3,55 +3,36 @@ const console = new Console();
 // --------------------------------------------/
 /// Autor: Manuel Rosendo Castro Iglesias.
 
-/// Primer ciclo "dijo que no usaramos no FOR() ni WHILE() ni DO WHILE()
+/// Primer ciclo "dijo que no usaramos ni FOR() ni WHILE() ni DO WHILE()
 
 const firstHours = console.readNumber('Dame las horas: ');
 const firstMinutes = console.readNumber('Dame los  minutos: ');
 const firstSeconds = console.readNumber('Dame los  segundos: ');
-//console.writeln(``);
+console.writeln();
+const secondHours = console.readNumber('Dame más Horas: ');
+const secondMinutes = console.readNumber('Dame más Minutos: ');
+const secondSeconds = console.readNumber('Dame más Segundos: ');
+console.writeln();
 
-const secondHours = console.readNumber('Dame las Horas: ');
-const secondMinutes = console.readNumber('Dame los Minutos: ');
-const secondSeconds = console.readNumber('Dame los Segundos: ');
-//console.writeln(``);
+// TODO: refactorizar eliminando variables ¿es necesario? Pero creo que esto es lo más legible.
+const addedSeconds = firstSeconds + secondSeconds;
+const residueSeconds = addedSeconds % 60;
+const minutesFromSeconds = (addedSeconds - residueSeconds) / 60;
 
-console.writeln(`La suma es ${firstHours - secondHours}:${firstMinutes - secondMinutes}:${firstSeconds - secondSeconds}`);
+const addedMinutes = firstMinutes + secondMinutes + minutesFromSeconds;
+const residueMinutes = addedMinutes % 60;
+const hoursFromMinutes = (addedMinutes - residueMinutes) / 60;
 
-// Para descomentar rapidamente en Visual Studio: Quitar o poner la primera / en  //*//
-//*//
-    ////   -- Importante no alterar el orden de las siguientes l�neas  --  --  --  --  --  --  --
-    const hours = firstHours + secondHours
-    const seconds = firstSeconds + secondSeconds;
-    seconds > 60 ? (minutes += (seconds - seconds % 60) / 60) : seconds = seconds % 60;
-    const minutes = firstMinutes + secondMinutes;
-    minutes > 60 ? (hours += (minutes - minutes % 60) / 60) : minutes = minutes % 60;
-    ////   -- Importante no alterar el orden de las l�neas precedentes --  --  --  --  --  --  --
-    console.writeln(`La suma es ${hours}:${minutes}:${seconds}`);
+
+/*// Para intecambiar código añadir barra al inicio.
+	/*/
+	const residueHours = firstHours + secondHours + hoursFromMinutes;
+	console.writeln(`La suma es ${residueHours}:${residueMinutes}:${residueSeconds}`);
+	/*/ // Era innecesario pero sale gratis.
+	const addedHours = firstHours + secondHours + hoursFromMinutes;
+	const residueHours = addedHours % 24;
+	const daysFromHours = (addedHours - residueHours) / 24;
+	console.writeln(`La suma es ${daysFromHours} días y ${residueHours}:${residueMinutes}:${residueSeconds}`);
 //*/
-/*//
-    console.writeln(``);
 
-    /// Segundo ciclo "dijo que no usaramos no FOR() ni WHILE() ni DO WHILE()
-
-    const firstHours2 = console.readNumber('Dame las horas: ');
-    const firstMinutes2 = console.readNumber('Dame los  minutos: ');
-    const firstSeconds2 = console.readNumber('Dame los  segundos: ');
-    //console.writeln(``);
-
-    const secondHours2 = console.readNumber('Dame las Horas: ');
-    const secondMinutes2 = console.readNumber('Dame los Minutos: ');
-    const secondSeconds2 = console.readNumber('Dame los Segundos: ');
-    //console.writeln(``);
-
-    ////   -- Importante no alterar el orden de las siguientes l�neas  --  --  --  --  --  --  --
-    const hours2 = firstHours2 + secondHours2
-    const seconds2 = firstSeconds2 + secondSeconds2;
-    seconds2 > 60 ? (minutes2 += (seconds2 - seconds2 % 60) / 60) : seconds2 = seconds2 % 60;
-    const minutes2 = firstMinutes2 + secondMinutes2;
-    minutes2 > 60 ? (hours2 += (minutes2 - minutes2 % 60) / 60) : minutes2 = minutes2 % 60;
-    ////   -- Importante no alterar el orden de las l�neas precedentes --  --  --  --  --  --  --
-    console.writeln(`La suma es ${hours2}:${minutes2}:${seconds2}`);
-
-    Coinsole.readstring()
-
-//*/
+console.writeln();
