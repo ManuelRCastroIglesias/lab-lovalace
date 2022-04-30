aee92745db9914346920847aab1110533170ec52
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-04-30 21:17:57 +0200

    Refactorizando 3-date\v1.js

--- tech-js-imperativa/3-date/v0/v1.js
+++ tech-js-imperativa/3-date/v0/v1.js
@@ -2,126 +2,124 @@ const { Console } = require("console-mpds");
 const console = new Console();
 // --------------------------------------------/
 /// Autor: Manuel Rosendo Castro Iglesias.
-/// Autora: María Paz López.
-/// Un ejercicio de lo más divertido.
+/// Autora: MarÃ­a Paz LÃ³pez.
+/// Un ejercicio de lo mÃ¡s divertido.
 /// ### 1-seasonWithPart/v0
 /// Hemos utilizado de todo: Ternario, While, Switch, 
-/// y esperamos a poner los nombres de los días y los meses
+/// y esperamos a poner los nombres de los dÃ­as y los meses
 /// para usar el Arrays, For y Do-While.
 
-/// OJO:  Hay un error aún, pero estoy cansado. Mañana sigo.
+/// OJO:  Hay un error aÃºn, pero estoy cansado. MaÃ±ana sigo.
 /// OJO:  No hacer cambios importantes cuando estas cansado.
-/// OJO:  ¡Por poco la lio parda!
+/// OJO:  !Por poco la lÃ­o parda!
 
-const yearNow = 2022;
-let day = 1;
+
+//*// // TODO: Crear objeto GlobalSettings() parÃ¡metros generales del programa.
+const yearNow = 2022; // Falta aÃ±adir mÃ©todo fecha actual.
+	// TODO: Crear mÃ©todo ResetSettings() variables.
+let day   = 1;
 let month = 1;
-let year = 0;
-let mySeasonDay = "";
-let mySeasonmonth = "";
-let mySeasonYear = "";
-let mySeasonText = "";
-let myEasterEggs = false;
-const TXT_EARLY = " cae a principios";
-const TXT_MIDDLE = " cae a mediados";
-const TXT_LATE = " cae a finales";
-const TXT_WINTER = " del Invierno";
-const TXT_SPRING = " de la Primavera";
-const TXT_SUMMER = " del Verano";
-const TXT_AUTUM = " del Otoño";
+let year  = 1;
+let mySeasonDay   = "";
+let mySeasonMonth = "";
+let mySeasonYear  = "";
+let mySeasonText  = "";
+let myEasterEggs  = false; // Crear mÃ©todo EasterEggs.
+const TXT_EARLY   = " cae a principios";
+const TXT_MIDDLE  = " cae a mediados";
+const TXT_LATE    = " cae a finales";
+const TXT_WINTER  = " del Invierno";
+const TXT_SPRING  = " de la Primavera";
+const TXT_SUMMER  = " del Verano";
+const TXT_AUTUM   = " del OtoÃ±o";
+//*/ // Fin de bloque definiciÃ³n de variables y constantes.
 
-while (myEasterEggs === false) {
+do {
+	//*// // TODO: Sustituir por mÃ©todo ReseetSettings()
+	day = 1;
+	month = 1;
+	year = 1;
+	mySeasonDay = "";
+	mySeasonMonth = "";
+	mySeasonYear = "";
+	mySeasonText = "";
+	myEasterEggs = false;
 
-	day = console.readNumber('Escriba un día (1-30): ');
+	day = console.readNumber('Escriba un dÃ­a (1-30): ');
 	month = console.readNumber('Escriba un mes (1-12): ');
-	year = console.readNumber('Escriba un año (… … … …): ');
+	year = console.readNumber('Escriba un aÃ±o (â€¦ â€¦ â€¦ â€¦): ');
 
 	if (day > 0) {
 		if (day <= 31) {
-			mySeasonDay = "El día " + day;
+			mySeasonDay = "El dÃ­a " + day;
 			myEasterEggs = true;
 		}
 		else {
-			console.writeln("Error en día")
+			console.write(mySeasonDay = " Error en dÃ­a");
+			/* ResetSettings() */
 			myEasterEggs = false;
 		}
 	}
-	//*// Por aquí hay un error. No cambia la variable myEasterEggs
+
 	if (month > 0) {
 		if (month <= 12) {
-			mySeasonmonth = " del mes " + month;
+			mySeasonMonth = " del mes " + month;
 			myEasterEggs = true;
 		}
 		else {
-			console.writeln("Error en mes")
+			console.write(mySeasonMonth = " Error en mes");
+			/* ResetSettings() */
 			myEasterEggs = false;
 		}
 	}
-	//*/
+
+	//*// Por aquÃ­ hay un error. No cambia la variable myEasterEggs
 	if (year > 0) {
 		if (year <= yearNow) {
-			mySeasonYear = " del año " + year;
+			mySeasonYear = " del aÃ±o " + year;
 			myEasterEggs = true;
 		}
 		else {
-			console.writeln("Error en año")
+			console.write(mySeasonYear = " Error en aÃ±o");
+			/* ResetSettings() */
 			myEasterEggs = false;
 		}
 	}
-}
-
+	//*/ //Fin del bloque que da error.
+} while (myEasterEggs === false);
+/*	Â¿PorquÃ© alÃ­neo los textos? Intenta averiguar si hay incosistencias en la siguiente lista.
+	En cualquier caso una vez terminado el cÃ³digo, se debe limpiar la basura al final.*/
 switch (x = month) {
-	case 1:
-		mySeasonText = TXT_EARLY + TXT_WINTER;
-		break;
-	case 2:
-		mySeasonText = TXT_MIDDLE + TXT_WINTER;
-		break;
-	case 3:
-		mySeasonText = day < 21 ? TXT_LATE + TXT_WINTER : TXT_EARLY + TXT_SPRING;
-		break;
-	case 4:
-		mySeasonText = TXT_EARLY + TXT_SPRING;
-		break;
-	case 5:
-		mySeasonText = TXT_MIDDLE + TXT_SPRING;
-		break;
-	case 6:
-		mySeasonText = day < 21 ? TXT_LATE + TXT_SPRING : TXT_EARLY + TXT_SUMMER;
-		break;
-	case 7:
-		mySeasonText = TXT_EARLY + TXT_SUMMER;
-		break;
-	case 8:
-		mySeasonText = TXT_MIDDLE + TXT_SUMMER;
-		break;
-	case 9:
-		mySeasonText = day < 21 ? TXT_LATE + TXT_SUMMER : TXT_EARLY + TXT_AUTUM;
-		break;
-	case 10:
-		mySeasonText = TXT_EARLY + TXT_AUTUM;
-		break;
-	case 11:
-		mySeasonText = TXT_MIDDLE + TXT_AUTUM;
-		break;
-	case 12:
-		mySeasonText = day < 21 ? TXT_LATE + TXT_SUMMER : TXT_LATE + TXT_WINTER;
-		break;
-	default:
-		console.writeln("exceptionCoitusInterruptus"); // My Easter Eggs
+	case  1: mySeasonText =				TXT_EARLY	+ TXT_WINTER; break;
+	case  2: mySeasonText =				TXT_MIDDLE	+ TXT_WINTER; break;
+	case  3: mySeasonText = day < 21 ?	TXT_LATE	+ TXT_WINTER :
+										TXT_EARLY	+ TXT_SPRING; break;
+	case  4: mySeasonText =				TXT_EARLY	+ TXT_SPRING; break;
+	case  5: mySeasonText =				TXT_MIDDLE	+ TXT_SPRING; break;
+	case  6: mySeasonText = day < 21 ?	TXT_LATE	+ TXT_SPRING :
+										TXT_EARLY	+ TXT_SUMMER; break;
+	case  7: mySeasonText =				TXT_EARLY	+ TXT_SUMMER; break;
+	case  8: mySeasonText =				TXT_MIDDLE	+ TXT_SUMMER; break;
+	case  9: mySeasonText = day < 21 ?	TXT_LATE	+ TXT_SUMMER :
+										TXT_EARLY	+ TXT_AUTUM; break;
+	case 10: mySeasonText =				TXT_EARLY	+ TXT_AUTUM; break;
+	case 11: mySeasonText =				TXT_MIDDLE	+ TXT_AUTUM; break;
+	case 12: mySeasonText = day < 21 ?	TXT_LATE	+ TXT_AUTUM :
+										TXT_LATE	+ TXT_WINTER; break;
+	default: console.writeln("exceptionCoitusInterruptus"); // My Easter Eggs
 }
 
-console.writeln(mySeasonDay + mySeasonmonth + mySeasonYear + mySeasonText + ".");
+console.writeln(`${mySeasonDay}${mySeasonMonth}${mySeasonYear}${mySeasonText}.`);
 
 
 
-// No me gusta esto de las llaves que abren en línea, pero parece que en JavaScript es la norma.
-// Ciclo While para solo permitir introducir fechas válidas. Ver TODO: yearNow.
-// Del usuario nunca me fío, ni nunca lo haré, ¡NO INSISTAN! 
-//	E incluso verificaría si intenta escribir la fecha como texto,
-//		pero de eso ya se encarga console.readNumber() ¡Gracias profe!
+// No me gusta esto de las llaves que abren en lÃ­nea, pero parece que en JavaScript es la norma.
+// Ciclo While para solo permitir introducir fechas vÃ¡lidas. Ver TODO: yearNow.
+// Del usuario nunca me fÃ³o, ni nunca lo harÃ©, Â¡NO INSISTAN! 
+//	E incluso verificarÃ­a si intenta escribir la fecha como texto,
+//		pero de eso ya se encarga console.readNumber() Â¡Gracias profe!
 //
-// TODO: nameOfTheYear * método Nombres del año ¿Método cantidad en letra?*
-// TODO: mySeason_month * nameOfThemonth * método Nombres del mes *
+// TODO: nameOfTheYear * mÃ©todo Nombres del aÃ±o Â¿MÃ©todo cantidad en letra?*
+// TODO: mySeason_month * nameOfThemonth * mÃ©todo Nombres del mes *
 // TODO: yearNow * Falta obtener dia y mes actual *
 
