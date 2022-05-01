78b920d6f93d9736c1b79eec14bf2235a72cb273
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-01 17:44:44 +0200

    cambios menores formato.

--- tech-js-imperativa/3-date/2-previous/2-previus.js
+++ tech-js-imperativa/3-date/2-previous/2-previus.js
@@ -3,6 +3,7 @@ const console = new Console();
 // --------------------------------------------/
 /// Autor: Manuel Rosendo Castro Iglesias.
 // Se consideran todos los meses de 30 días.
+/// Recuperado antiguo y estaba MUY MAL.
 // ### 3-date/2-previus
 const day = console.readNumber( 'Escriba el dia ( 1-30): ' );
 const month = console.readNumber( 'Escriba el mes ( 1-12): ' );
@@ -13,7 +14,16 @@ const yearSmallvalue = year - 1 < 1;      // false
 const monthLargeValue = month > 12;       // false
 const dayLargeValue = day > 30;           // false
 const yearLargeValue = year > 99;         // false
-const dateValid = daySmallvalue === monthSmallvalue === yearSmallvalue === dayLargeValue === monthLargeValue === yearLargeValue; /* sirve como prueba */
+
+const dateValid =
+	daySmallvalue ===
+	monthSmallvalue ===
+	yearSmallvalue ===
+	dayLargeValue ===
+	monthLargeValue ===
+	yearLargeValue;
+
+/* sirve como prueba */
 // ¡No sé como hacerlo sin Let! Me rindo.
 let dayPreviusSmall = day;
 let monthPreviusSmall = month;
@@ -23,12 +33,12 @@ let yearPreviusSmall = year;
 ( month === day === year === 1 ) ?
 	yearPreviusSmall = year - 1 :
 	( month === day === 1 ?
-		dayPreviusSmall = month - 1 :
+		(dayPreviusSmall = month - 1 ):
 		( day === 1 ) ?
-			monthPreviusSmall = day + 29 :
+			dayPreviusSmall = day + 29 :
 				dayPreviusSmall = day - 1 );
 
-const myDate = dateValid ?
+dateValid ?
 	console.writeln( `La fecha ` + day + `/` + month + `/` + year + ` y la anterior fecha es ` + dayPreviusSmall + `/` + monthPreviusSmall + `/` + yearPreviusSmall ) :
 	console.writeln( `La fecha ` + day + `/` + month + `/` + year + ` No puedo hacer ese Cálculo o la fecha no es válida` );
 console.writeln( `La fecha ` + day + `/` + month + `/` + year + ` No puedo hacer ese Cálculo o la fecha no es válida` );
