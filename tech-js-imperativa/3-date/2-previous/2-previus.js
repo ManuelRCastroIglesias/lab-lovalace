15bb028f7b14f0a92c95d2138d0b2caa9bbe973f
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-02 00:48:15 +0200

    ¡Por fin funciona!

--- tech-js-imperativa/3-date/2-previous/2-previus.js
+++ tech-js-imperativa/3-date/2-previous/2-previus.js
@@ -4,7 +4,6 @@ const console = new Console();
 /// Autor: Manuel Rosendo Castro Iglesias.
 /// Se consideran todos los meses de 30 días.
 // ### 3-date/2-previus
-/// 
 
 const day = console.readNumber( 'Escriba el dia (1-30):  ' );
 const month = console.readNumber( 'Escriba el mes (1-12):  ' );
@@ -19,19 +18,48 @@ const yearSmallvalue = year < 0; // false
 const yearLargeValue = year > 99; // false
 /* con la misma lógica se pueden calcular otros valores. */
 
-const dateValid =
-    daySmallvalue ===
+let dateValid2 =
+1 * (daySmallvalue ===
     dayLargeValue ===
     monthSmallvalue ===
     monthLargeValue ===
     yearSmallvalue ===
-    yearLargeValue;
+    yearLargeValue);
 /* sirve como prueba */
 
+const firstDayCE = ( ( ( 1 === day ) && 1 === month ) && 1 === year );
+const theNewYearDay = ( ( ( 1 === day) && 1 === month) && 1 !== year);
+const firstDayOfMonth = (1=== day) && ( month !== 1 );
 
+if ( firstDayCE)
+{
+    previusDay = day ;
+    previusMonth = month ;
+    previusYear = year;
+    dateValid2 = 0; 
+}
+else
+    if ( theNewYearDay )
+    {
+        previusDay = day + 29;
+        previusMonth = month + 11;
+        previusYear = year - 1;
+    }
+    else
+        if ( firstDayOfMonth)
+        {
+            previusDay = day + 29;
+            previusMonth = month - 1;
+            previusYear = year;
+        }
+        else
+        {
+            previusDay = day -1;
+            previusMonth = month;
+            previusYear = year;
+        }
 
-
-dateValid ?
-    console.writeln( `La fecha ${ day }/${ month }/${ year } sí es una fecha válida` ) :
+( dateValid2 === 1) ?
+    console.writeln( `La fecha ${ day }/${ month }/${ year } y la siguiente es ${ previusDay }/${ previusMonth }/${ previusYear }` ) :
     console.writeln( `La fecha ${ day }/${ month }/${ year } no es una fecha válida` );
 console.writeln();
