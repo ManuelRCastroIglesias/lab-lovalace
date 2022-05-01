bc9ea25240d365fb8cace06e5dbf08dae1aaf7cb
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-01 02:46:50 +0200

    Refactorizacio y archivo Léame.

--- tech-js-imperativa/3-date/2-previous/2-previus.js
+++ tech-js-imperativa/3-date/2-previous/2-previus.js
@@ -8,18 +8,16 @@ const console = new Console();
 
 // Se consideran todos los meses de 30 días.
 
-const day   = console.readNumber('Escriba el dia ( 1-30): ');
-const month = console.readNumber('Escriba el mes ( 1-12): ');
-const year  = console.readNumber('Escriba el año (01-99): ');
-
-const daySmallvalue     =  day      <1   ;            // false
-const monthSmallvalue   =  month    <1   ;            // false
-const yearSmallvalue    =  year  -1 <1   ;            // false
-const monthLargeValue   =  month    >12  ;            // false
-const dayLargeValue     =  day      >30  ;            // false
-const yearLargeValue    =  year     >99  ;            // false
-
-const dateValid = daySmallvalue === monthSmallvalue === yearSmallvalue === dayLargeValue === monthLargeValue === yearLargeValue; /* sirve como prueba */
+let  day   = console.readNumber('Escriba el dia ( 1-30): ');
+let  month = console.readNumber('Escriba el mes ( 1-12): ');
+let  year  = console.readNumber('Escriba el año (01-99): ');
+let  daySmallvalue     =  day      <1   ;            // false
+let  monthSmallvalue   =  month    <1   ;            // false
+let  yearSmallvalue    =  year  -1 <1   ;            // false, esto evita el año 0
+let  monthLargeValue   =  month    >12  ;            // false
+let  dayLargeValue     =  day      >30  ;            // false
+let  yearLargeValue    =  year     >99  ;            // false
+let  dateValid = daySmallvalue === monthSmallvalue === yearSmallvalue === dayLargeValue === monthLargeValue === yearLargeValue; /* sirve como prueba */
 
 // ¡No sé como hacerlo sin Let! Me rindo.
 let dayPreviusSmall = day;    
@@ -27,16 +25,42 @@ let monthPreviusSmall = month;
 let yearPreviusSmall = year;  
 
 // Lógica -- datePrevius --
-(month === day === year === 1) ? 
-	yearPreviusSmall = year-1: 
-		(month === day === 1 ? 
-			 dayPreviusSmall = month-1 : 
-				(day === 1) ? 
-					monthPreviusSmall = day+29 : 
-						dayPreviusSmall = day-1);
 
-const myDate = dateValid ? 
-console.writeln(`La fecha `+day+`/`+month+`/`+year+` y la anterior fecha es `+dayPreviusSmall+`/`+monthPreviusSmall+`/`+yearPreviusSmall) : 
-	console.writeln(`La fecha `+day+`/`+month+`/`+year+` No puedo hacer ese Cálculo o la fecha no es válida`);
+
+if (!(year === month === day === 1))
+{
+	yearPreviusSmall = year - 1;
+	monthPreviusSmall = month - 1;
+	dayPreviusSmall = day + 29;
+}
+else if (month === day === 1)
+{
+	dayPreviusSmall = day + 29 ;
+	monthPreviusSmall = month + 11;
+}
+else if (day !== 1)
+{
+	dayPreviusSmall = day - 1;
+}
+else{
+	ErrorInDate = true;
+}
+
+
+
+
+
+
+//!(year === month === day === 1) ?                 
+//	yearPreviusSmall = year-1:                    
+//	!(month === day === 1 ?                       
+//			dayPreviusSmall = month-1 :           
+//				(day === 1) ?					  
+//					monthPreviusSmall = day+29 :  
+//						dayPreviusSmall = day-1); 
+
+//dateValid ? 
+console.writeln(`La fecha ${day}/${month}/${year} y la anterior fecha es ${dayPreviusSmall}/${monthPreviusSmall}/${yearPreviusSmall}`);
+//	console.writeln(`La fecha `+day+`/`+month+`/`+year+` No puedo hacer ese Cálculo o la fecha no es válida`);
 
 // console.readString('text');
---
+++ tech-js-imperativa/3-date/2-previous/ReadMe.md
@@ -0,0 +1,68 @@
+# 3-date/2-previous
+>>~~~
+>>Dame el día: 12
+>>Dame el mes: 12
+>>Dame el año: 12
+>>
+>>La fecha 12/12/12 y la siguiente es 11/12/12
+>>~~~  
+>>---  
+>>~~~
+>>Dame el día: 1
+>>Dame el mes: 1
+>>Dame el año: 12
+>>
+>>La fecha 30/1/12 y la siguiente es 30/12/11
+>>~~~
+>>---  
+>>~~~
+>>Dame el día: 1
+>>Dame el mes: 4
+>>Dame el año: 12
+>>
+>>La fecha 1/4/12 y la siguiente es 30/3/12
+>>~~~ 
+>>--- 
+>>~~~ 
+>> Mi Prueba adicional.
+>>Dame el día: 1
+>>Dame el mes: 1
+>>Dame el año: 1
+>> --> Debe volver a pedir el dato. El año 0 no existe.
+>>Dame el día: 36
+>>Dame el mes: 361
+>>Dame el año: 1
+>> --> Debe volver a pedir el dato. día >30
+>>Dame el día: 1
+>>Dame el mes: 36
+>>Dame el año: 1
+>> --> Debe volver a pedir el dato. mes > 12
+>>Dame el día: 1
+>>Dame el mes: 1
+>>Dame el año: 100
+>> --> Debe volver a pedir el dato.año > 99
+>>~~~ 
+>>---  
+>--- 
+> ## Autores:  
+>>--- 
+>> * Manuel Rosendo Castro Iglesias.  
+>> * María Paz López.  
+>> --- 
+>--- 
+> ## Algoritmo del día prévio. (aún no va)
+>>--- 
+>>~~~
+>>Si  !(year  === month === day === 1) : Cambiamos el dia> >a (dia+29), el mes a (mes-1) y el año a (año-1);
+>>Si  (dia === 1  y mes === 1)         : Cambiamos el dia> >a (dia+29), el mes a (mes-1);
+>>Si  (dia !== 1  y mes !== 1)         : Cambiamos el dia> >a (dia-1);
+>>Si  (year  === month === day === 1)  : Esta fecha rstá >>mal capturada.
+>>
+>>If    !(year  === month === day === 1){year  -= 1 ; >>month -= 1 ; day += 29}; // ¡Es el 01/01/01!
+>>ElseIf (month === day === 1)          { day += 29 ; >>month -= 1 ;          }; // (year  !== 1)
+>>ElseIf (day   === 1)                  {day   -= >>1 ;                       }; // (month !== 1)
+>>Else   ErrorInDate = >true;                            >                     >  // (day   !== 1)  
+>>~~~
+>>---
+>---
+---
