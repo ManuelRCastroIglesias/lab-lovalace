2319e86a1ec6b7b272a638acaab93adfede21be6
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-01 22:50:14 +0200

    Añadido salto de línea

--- tech-js-imperativa/3-date/0-valid/0-valid.js
+++ tech-js-imperativa/3-date/0-valid/0-valid.js
@@ -2,13 +2,13 @@ const { Console } = require("console-mpds");
 const console = new Console();
 // --------------------------------------------/
 /// Autor: Manuel Rosendo Castro Iglesias.
-
 // Se consideran todos los meses de 30 días.
 // ### 3-date/0-valid
 
 const day = console.readNumber('Escriba el dia (1-30):  ');
 const month = console.readNumber('Escriba el mes (1-12):  ');
-const year = console.readNumber('Escriba el año (01-99):  ');
+const year = console.readNumber( 'Escriba el año (01-99):  ' );
+console.writeln();
 
 const daySmallvalue = day <1 ; // false
 const dayLargeValue = day >30 ; // false
