683974b91ee6a6c0edc7a275115950838353acff
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-01 22:07:41 +0200

    Ejercico 2-coordinate/2-isHorizontal.

--- tech-js-imperativa/2-coordinate/2-isHorizontal/2-isHorizontal.js
+++ tech-js-imperativa/2-coordinate/2-isHorizontal/2-isHorizontal.js
@@ -1,5 +1,25 @@
 const { Console } = require("console-mpds");
 const console = new Console();
-// --------------------------------------------/
+/// --------------------------------------------/
 /// <Autor:> Manuel Rosendo Castro Iglesias.
+/// <Ejercicio:> 2-coordinate/2-isHorizontal.
+///  https://github.com/USantaTecla-0-domains/0-simpleDomains/blob/master/docs/5-units.md#2-coordinate2-ishorizontal
+/// --------------------------------------------/
 
+/// ----------------------------------------------------------------------------------
+console.writeln( `Coordenada origen:` );
+const abscissaOrigin = console.readNumber( `Dame la abcisa de la coordenada:` );
+const abscisaDestination = console.readNumber( `Dame la abcisa de la coordenada:` );
+console.writeln( `Coordenada destino:` );
+const orderedOrigin = console.readNumber( `Dame la abcisa de la coordenada:` );
+const orderedDestination = console.readNumber( `Dame la abcisa de la coordenada:` );
+/// ----------------------------------------------------------------------------------
+if ( abscissaOrigin === abscisaDestination )
+{
+    console.writeln (`La coordenada origen (${ abscissaOrigin }, ${ abscisaDestination }) y la coordenada destino (${ orderedOrigin }, ${ orderedDestination } si es un movimiento horizontal`);
+}
+else
+{
+    console.writeln (`La coordenada origen (${ abscissaOrigin }, ${ abscisaDestination }) y la coordenada destino (${ abscisaDestination }, ${ orderedDestination } no es un movimiento horizontal)`);
+}
+/// ----------------------------------------------------------------------------------
