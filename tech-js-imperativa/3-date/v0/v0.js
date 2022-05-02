cbe9e0022fd11a96a537b18635deb31fe19c73c3
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-02 02:18:40 +0200

    Rectificado archivo ejercicio. Sin experimentos raros.

--- tech-js-imperativa/3-date/v0/v0.js
+++ tech-js-imperativa/3-date/v0/v0.js
@@ -2,52 +2,47 @@
 const console = new Console();
 // --------------------------------------------/
 /// Autor: Manuel Rosendo Castro Iglesias.
-
 // ### 1-seasonWithPart/v0
-
 // TODO: yearNow * Falta verificar dia y mes actual *
+
 const yearNow =2022;
 
-const day   = console.readNumber ('Escriba un día (1-30): ');
+const day = console.readNumber ('Escriba un día (1-30): ');
 const month = console.readNumber ('Escriba un mes (1-12): ');
-const year  = console.readNumber ('Escriba un año (… … … …): '); 
+const year = console.readNumber ('Escriba un año (… … … …): '); 
 
-// TODO: mySeasons      * Crear bucle reintento con valores false *
-const mySeason_Day   = 	day   > 0 ? (day   <= 31      ? "El día " + day   : false ) : false ;
-// TODO: mySeason_Month * Si hay que nombrar el mes corregir el texto *
-// TODO: nameOfTheMonth * método Nombres del mes *
-const mySeason_Month = month > 0 ? (month <= 12      ? " del mes " + month : false ) : false ;
-// TODO: mySeason_Year  * Si hay que nombrar el año corregir el texto *
-// TODO: nameOfTheYear  * método Nombres del año ¿Método cantidad en letra?*
-const mySeason_Year  = year  > 0 ? (year  <= yearNow ? " del año " + year  : false ) : false ;
+// TODO: mySeasons * Crear bucle reintento con valores false *
+const myDay = day > 0 ? (day <= 31 ? "El día " + day : false ) : false ;
+// TODO: mySeasonMonth * Si hay que nombrar el mes, corregir el texto *
+// TODO: nameOfTheMonth * método Nombres del mes  ¿Método cantidad en letra?*
+const myMonth = month > 0 ? (month <= 12 ? " del mes " + month : false ) : false ;
+// TODO: mySeasonYear * Si hay que nombrar el año, corregir el texto *
+// TODO: nameOfTheYear * método Nombres del año ¿Método cantidad en letra?*
+const myYear = year > 0 ? (year <= yearNow ? " del año " + year : false ) : false ;
 
-const aboutEarly_Text = " cae a principios";
-const aboutMiddleText = " cae a mediados";
-const aboutLate__Text = " cae a finales";
+const Early = " cae a principios";
+const Middle = " cae a mediados";
+const Late = " cae a finales";
 
-const winterSeasonText = " del Invierno";
-const springSeasonText = " de la Primavera";
-const summerSeasonText = " del Verano";
-const autumnSeasonText = " del Otoño";
+const winter = " del Invierno";
+const spring = " de la Primavera";
+const summer = " del Verano";
+const autumn = " del Otoño";
+// TODO: Crear array en cuanto se pueda.
+const mySeason =
+month === 1 ? ( day < 21 ? Early + winterTxt : Middle + winter ) :
+month === 2 ?( day < 21 ? Middle + winterTxt : Late + winter ) :
+month === 3 ?( day < 21 ? Late + winterTxt : Early + spring ) :
+month === 4 ?( day < 21 ? Early + springTxt : Middle + spring ) :
+month === 5 ?( day < 21 ? Middle + springTxt : Late + spring ) :
+month === 6 ?( day < 21 ? Late + springTxt : Early + summer ) :
+month === 7 ?( day < 21 ? Early + summerTxt : Middle + summer ) :
+month === 8 ?( day < 21 ? Middle + summerTxt : Late + summer ) :
+month === 9 ?( day < 21 ? Late + summerTxt : Early + autumn ) :
+month === 10 ?( day < 21 ? Early + autumnTxt : Middle + autumn ) :
+month === 11 ?( day < 21 ? Middle + autumnTxt : Late + autumn ) :
+( day < 21 ? Late + autumnTxt : Early + winter );
 
-const mySeasonText = 
-		 month ===  1 ?  aboutEarly_Text + winterSeasonText: 
-		 month ===  2 ?  aboutMiddleText + winterSeasonText: 
-		 month ===  3 ?  
-			(day < 21 ? aboutLate__Text + winterSeasonText : aboutEarly_Text + springSeasonText):
-		 month ===  4 ?  aboutEarly_Text + springSeasonText: 
-		 month ===  5 ?  aboutMiddleText + springSeasonText: 
-		 month ===  6 ?  
-			(day < 21 ? aboutLate__Text + springSeasonText : aboutEarly_Text + summerSeasonText):
-		 month ===  7 ?  aboutEarly_Text + summerSeasonText: 
-		 month ===  8 ?  aboutMiddleText + summerSeasonText: 
-		 month ===  9 ?  
-			(day < 21 ? aboutLate__Text + summerSeasonText : aboutEarly_Text + autumnSeasonText):
-		 month === 10 ?  aboutEarly_Text + autumnSeasonText: 
-		 month === 11 ?  aboutMiddleText + autumnSeasonText: 
-			(day < 21 ? aboutLate__Text + summerSeasonText : abouteLate_Text + winterSeasonText);
+console.writeln( `${ myDay }${ myMonth }${ myYear }${ mySeason }.` );
 
-console.writeln(mySeason_Day+mySeason_Month+mySeason_Year+mySeasonText+".");
-
-
-						
+console.writeln();
