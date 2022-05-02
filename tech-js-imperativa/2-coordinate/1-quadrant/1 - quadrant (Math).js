98d0453885782203e9068250e40b990faa65149c
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-02 13:59:59 +0200

    Formato final

--- tech-js-imperativa/2-coordinate/1-quadrant/1 - quadrant (Math).js
+++ tech-js-imperativa/2-coordinate/1-quadrant/1 - quadrant (Math).js
@@ -1,36 +1,37 @@
 const { Console } = require("console-mpds");
 const console = new Console();
-// --------------------------------------------/
-/// <Autor:> Manuel Rosendo Castro Iglesias.
-/// 0-onAxes\1-quadrant (v.Math)
-// --------------------------------------------/
-    let X=0;    // abcissas            //
-    let Xi = 0; // relative abcissas   //
-    let Y = 0;  // ordinates           //
-    let Yi = 0; // relative ordinates  //
-    let XY = 0; // quadrant            //
-    // first  quadrant ( I   ) = +++ ; //
-    // second quadrant ( II  ) =  -  ; //
-    // third  quadrant ( III ) = --- ; //
-    // fourth quadrant ( IV  ) =  +  ; //
-// --------------------------------------------/
-    X = + console.readNumber( "Dame la abscisa de la coordenada: ");
-    Y = + console.readNumber("Dame la ordenada de la coordenada: ");
-console.writeln();
-    (Xi) = (X > 0) ? 1 : (X < 0) ? -1 : 0;
-    (Yi) = (Y > 0) ? 1 : (Y < 0) ? -1 : 0;
-    XY = (Xi + Xi + Yi);
-on_XY =
-    ( XY ) === +3 ? " está en el primer cuadrante" :
-    ( XY ) === -1 ? " está en el segundo cuadrante":
-    ( XY ) === -3 ? " está en el tercer cuadrante" :
-    ( XY ) === -1 ? " está en el cuarto cuadrante" : false;
-txt_on_XY_axis =
-    (X === 0) && (Y === 0) ? " está sobre el origen de coordenadas" :
-    (X !== 0) && (Y !== 0) ? on_XY :
-    (X === 0) && (Y !== 0) ? " está sobre el eje de abscisa" :
-    (X !== 0) && (Y === 0) ? " está sobre el eje de ordenadas" : "";
-console.writeln( `La coordenada (${ X }, ${ Y })"${ txt_on_XY_axis }.` );
-console.writeln();
+/// -----------------------------------------------------------------------//
+/// <Autor:> Manuel Rosendo Castro Iglesias.                               //
+/// 0-onAxes\1-quadrant (v.Math)                                           //
+/// -----------------------------------------------------------------------//
+    let X=0;    // abcissas                                                //
+    let Xi = 0; // relative abcissas                                       //
+    let Y = 0;  // ordinates                                               //
+    let Yi = 0; // relative ordinates                                      //
+    let XY = 0; // quadrant                                                //
+    // first  quadrant ( I   ) = +++ ;                                     //
+    // second quadrant ( II  ) =  -  ;                                     //
+    // third  quadrant ( III ) = --- ;                                     //
+    // fourth quadrant ( IV  ) =  +  ;                                     //
+/// -----------------------------------------------------------------------//
+    X = + console.readNumber( "Dame la abscisa de la coordenada: " );      //
+    Y = + console.readNumber( "Dame la ordenada de la coordenada: " );     //
+console.writeln();                                                         //
+    ( Xi ) = ( X > 0 ) ? 1 : ( X < 0 ) ? -1 : 0;                           //
+    ( Yi ) = ( Y > 0 ) ? 1 : ( Y < 0 ) ? -1 : 0;                           //
+    ( XY ) = ( Xi + Xi + Yi );                                             //
+on_XY =                                                                    //
+    ( XY ) === +3 ? " está en el primer cuadrante" :                       //
+    ( XY ) === -1 ? " está en el segundo cuadrante" :                      //
+    ( XY ) === -3 ? " está en el tercer cuadrante" :                       //
+    ( XY ) === -1 ? " está en el cuarto cuadrante" : false;                //
+txt_on_XY_axis =                                                           //
+    ( X === 0 ) && ( Y === 0 ) ? " está sobre el origen de coordenadas" :  //
+    ( X !== 0 ) && ( Y !== 0 ) ? on_XY :                                   //
+    ( X === 0 ) && ( Y !== 0 ) ? " está sobre el eje de abscisa" :         //
+    ( X !== 0 ) && ( Y === 0 ) ? " está sobre el eje de ordenadas" : "";   //
+console.writeln( `La coordenada (${ X }, ${ Y })"${ txt_on_XY_axis }.` );  //
+console.writeln();                                                         //
+/// -----------------------------------------------------------------------//
 
 
