a978e66c1a9ba14893708f8a212da4460b816bd5
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-03 15:25:44 +0200

    1ª refactorización.
    Se creó esqueleto switch-case.

--- tech-js-imperativa/3-date/v0/v0.js
+++ tech-js-imperativa/3-date/v0/v0.js
@@ -1,10 +1,12 @@
 ﻿const { Console } = require("console-mpds");
 const console = new Console();
-// --------------------------------------------/
-/// Autor: Manuel Rosendo Castro Iglesias.
-// ### 1-seasonWithPart/v0
-// TODO: yearNow * Falta verificar dia y mes actual *
+/// --------------------------------------------/
+/// <Autor: Manuel Rosendo Castro Iglesias>
+/// <Title: ### 1-seasonWithPart/v0>
+/// Se consideran todos los meses de 30 días.
+/// --------------------------------------------/
 
+// TODO: yearNow * Falta verificar dia y mes actual *
 const yearNow =2022;
 
 const day = console.readNumber ('Escriba un día (1-30): ');
@@ -12,36 +14,81 @@ const month = console.readNumber ('Escriba un mes (1-12): ');
 const year = console.readNumber ('Escriba un año (… … … …): '); 
 
 // TODO: mySeasons * Crear bucle reintento con valores false *
-const myDay = day > 0 ? (day <= 31 ? "El día " + day : false ) : false ;
+const myDay = day > 0 ? (day <= 31 ? "El día " + day : false) : false;
 // TODO: mySeasonMonth * Si hay que nombrar el mes, corregir el texto *
 // TODO: nameOfTheMonth * método Nombres del mes  ¿Método cantidad en letra?*
-const myMonth = month > 0 ? (month <= 12 ? " del mes " + month : false ) : false ;
+const myMonth = month > 0 ? (month <= 12 ? " del mes " + month : false) : false;
 // TODO: mySeasonYear * Si hay que nombrar el año, corregir el texto *
 // TODO: nameOfTheYear * método Nombres del año ¿Método cantidad en letra?*
-const myYear = year > 0 ? (year <= yearNow ? " del año " + year : false ) : false ;
+const myYear = year > 0 ? (year <= yearNow ? " del año " + year : false) : false;
 
-const Early = " cae a principios";
-const Middle = " cae a mediados";
-const Late = " cae a finales";
+const EARLY = " cae a principios";
+const MIDDLE = " cae a mediados";
+const LATE = " cae a finales";
 
-const winter = " del Invierno";
-const spring = " de la Primavera";
-const summer = " del Verano";
-const autumn = " del Otoño";
+const WINTER = " del Invierno";
+const SPRING = " de la Primavera";
+const SUMMER = " del Verano";
+const AUTUMN = " del Otoño";
+
 // TODO: Crear array en cuanto se pueda.
+
+/*// Refacroring: Whith switch case:
+switch(month){
+    case 1:
+        
+    return;
+    case 2:
+        
+    return;
+    case 3:
+        
+    return;
+    case 4:
+        
+    return;
+    case 5:
+        
+    return;
+    case 6:
+        
+    return;
+    case 7:
+        
+    return;
+    case 8:
+        
+    return;
+    case 9:
+        
+    return;
+    case 10:
+        
+    return;
+    case 11:
+        
+    return;
+    case 12:
+        
+    return;
+    default:
+        ;
+    }
+/*/ // Refacroring: Whith switch case:
 const mySeason =
-month === 1 ? ( day < 21 ? Early + winterTxt : Middle + winter ) :
-month === 2 ?( day < 21 ? Middle + winterTxt : Late + winter ) :
-month === 3 ?( day < 21 ? Late + winterTxt : Early + spring ) :
-month === 4 ?( day < 21 ? Early + springTxt : Middle + spring ) :
-month === 5 ?( day < 21 ? Middle + springTxt : Late + spring ) :
-month === 6 ?( day < 21 ? Late + springTxt : Early + summer ) :
-month === 7 ?( day < 21 ? Early + summerTxt : Middle + summer ) :
-month === 8 ?( day < 21 ? Middle + summerTxt : Late + summer ) :
-month === 9 ?( day < 21 ? Late + summerTxt : Early + autumn ) :
-month === 10 ?( day < 21 ? Early + autumnTxt : Middle + autumn ) :
-month === 11 ?( day < 21 ? Middle + autumnTxt : Late + autumn ) :
-( day < 21 ? Late + autumnTxt : Early + winter );
+month === 1  ? (day < 21 ? EARLY  + WINTERTxt : MIDDLE + WINTER) :
+month === 2  ? (day < 21 ? MIDDLE + WINTERTxt : LATE   + WINTER) :
+month === 3  ? (day < 21 ? LATE   + WINTERTxt : EARLY  + SPRING) :
+month === 4  ? (day < 21 ? EARLY  + SPRINGTxt : MIDDLE + SPRING) :
+month === 5  ? (day < 21 ? MIDDLE + SPRINGTxt : LATE   + SPRING) :
+month === 6  ? (day < 21 ? LATE   + SPRINGTxt : EARLY  + SUMMER) :
+month === 7  ? (day < 21 ? EARLY  + SUMMERTxt : MIDDLE + SUMMER) :
+month === 8  ? (day < 21 ? MIDDLE + SUMMERTxt : LATE   + SUMMER) :
+month === 9  ? (day < 21 ? LATE   + SUMMERTxt : EARLY  + AUTUMN) :
+month === 10 ? (day < 21 ? EARLY  + AUTUMNTxt : MIDDLE + AUTUMN) :
+month === 11 ? (day < 21 ? MIDDLE + AUTUMNTxt : LATE   + AUTUMN) :
+               (day < 21 ? LATE   + AUTUMNTxt : EARLY  + WINTER) ;
+//*/  // End refactoring block. (Se mantiene formato tabular para verificación.)
 
 console.writeln( `${ myDay }${ myMonth }${ myYear }${ mySeason }.` );
 
