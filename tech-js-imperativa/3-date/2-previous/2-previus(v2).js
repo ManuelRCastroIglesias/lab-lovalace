706938b71d4aca42777e0503df9446d035a5c826
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-03 14:51:20 +0200

    0-valid 1ª Refactorización. 2-Previus(v2) Ajustes formato.

--- tech-js-imperativa/3-date/2-previous/2-previus(v2).js
+++ tech-js-imperativa/3-date/2-previous/2-previus(v2).js
@@ -8,25 +8,25 @@ const console = new Console();
 
 let day = 1;
 do{
-    day = console.readNumber( 'Escriba el dia (1-30):  ' );
+    day = console.readNumber( 'Escriba el dia (1-30): ' );
 } while ( day < 1 || day > 30 );
 let month = 1;
 do {
-    month = console.readNumber( 'Escriba el mes (1-12):  ' );
+    month = console.readNumber( 'Escriba el mes (1-12): ' );
 } while ( month < 1 || month > 12 );
 let year =1;
 do {
-    year = console.readNumber( 'Escriba el año (01-99):  ' );
+    year = console.readNumber( 'Escriba el año (01-99): ' );
 } while ( year < 1 || year > 99 );
 console.writeln();
 
-if (((day === 1) && month === 1) && year !== 1) {   /* Is it the  the New Year'Day ? */
+if (((day === 1) && month === 1) && year !== 1) { /* Is it the the New Year'Day ? */
     previusDay = day + 29;
     previusMonth = month + 11;
     previusYear = year - 1;
 }
 else
-    if ((1 === day) && (month !== 1)) {   /* Is it the  first Day Of Month ? */
+    if ((1 === day) && (month !== 1)) { /* Is it the first Day Of Month ? */
         previusDay = day + 29;
         previusMonth = month - 1;
         previusYear = year * 1;
@@ -38,6 +38,9 @@ else
     }
 
 (((day === 1) && month === 1) && year === 1) ? /* Is it the first day of the E.C.? */
-    console.writeln(`La fecha ${day}/${month}/${year} y la siguiente es ${previusDay}/${previusMonth}/${previusYear}`) :
-    console.writeln(`La fecha ${day}/${month}/${year} no es una fecha válida`);
-console.writeln();
+    console.writeln(`\
+        La fecha ${day}/${month}/${year} \
+        y la siguiente es ${previusDay}/${previusMonth}/${previusYear}`) :
+    console.writeln(`\
+        La fecha ${day}/${month}/${year} no es una fecha válida`);
+    console.writeln();
