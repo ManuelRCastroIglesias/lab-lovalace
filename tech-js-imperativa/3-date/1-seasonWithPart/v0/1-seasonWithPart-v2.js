9ad210521bd14cdce2bef2ea4e56c12eb9e437ea
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-05 19:24:16 +0200

    SE hacen cambis casi finales y se borran archivos innecesarios.

--- tech-js-imperativa/3-date/1-seasonWithPart/v0/1-seasonWithPart-v2.js
+++ tech-js-imperativa/3-date/1-seasonWithPart/v0/1-seasonWithPart-v2.js
@@ -1,34 +1,31 @@
-﻿const { Console } = require("console-mpds");
-const console = new Console();
-//// --------------------------------------------/
-/// <Autor: Manuel Rosendo Castro Iglesias.>
-/// <Autor: Alberto Antelo>
-// ### 1-seasonWithPart/v0
-// Da error con fechas fuera de rango, tengo que corregirlo.
-// Una vez esté terminado se eliminarán comentarios innecesarios.
-/// --------------------------------------------/
+﻿
+///// --------------------------------------------------------------------//
+		const { Console } = require("console-mpds");                      //
+		const console = new Console();                                    //
+{//// --------------------------------------------------------------------//
+	/// <Autor: Manuel Rosendo Castro Iglesias.>                          //
+	/// <Autor: Alberto Antelo>                                           //
+	/// <Titulo: ### 1-seasonWithPart/v0/ >                               //
+	/// ------------------------------------------------------------------//
+} // Créditos:
 
-
-/// TODO: yearNow * Falta verificar dia y mes actual *;
-
-const yearNow = 2022 // para verificar aún falta llegar a funciones de fecha y hora.
-//tienes que declarar lo de abajo con let, sino no te deja luego guardar los datos, estan declaradas como constantes
+const yearNow = 2022 
 let day = 1;
 let month = 1;
 let year =1;
 
-	// No es necesario añadir " + " para forzar tipo ya que solo acepta enteros
-	// ¡Y esta reforzada la reescritura de const
-	// Si este comportamiento cambia en el futuro será neesario ver posible solución.
 do{
 	day = console.readNumber( 'Escriba un día (1-30): ' );}
-while ( day < 1 || day >= 30 );    // ¿Añadir verificación dias 31 o 28/29? ¡Interesante!
-do{
+while ( day < 1 || day >= 30 );
+do {
+	console.writeln();
 	month = console.readNumber( 'Escriba un mes (1-12): ' );}
-while ( month < 1 || month > 12 ); // TODO: nameOfTheMonth, ¿crear método Nombres del mes?
-do{
+while ( month < 1 || month > 12 );
+do {
+	console.writeln();
 	year = console.readNumber( 'Escriba un año (… … … …): ' );}
-while ( year < 1 || year > yearNow );
+while (year < 1 || year > yearNow);
+	console.writeln();
 
 
 const aboutEarlyTxt   = " cae a principios";
@@ -39,8 +36,6 @@ const winterSeasonTxt = " del Invierno";
 const springSeasonTxt = " de la Primavera";
 const summerSeasonTxt = " del Verano";
 const autumnSeasonTxt = " del Otoño";
-
-
 
 const mySeasonTxt =
 	month ===  1 ?  aboutEarlyTxt + winterSeasonTxt :
@@ -63,5 +58,3 @@ const mySeasonTxt =
 					aboutEarlyTxt + winterSeasonTxt);
 
 console.writeln(`El día ${day} del ${month} del año ${year}/${mySeasonTxt}.`);
-
-// Ahora que seta verificado le quito toda la sangria para el profe LÑuius. :)
