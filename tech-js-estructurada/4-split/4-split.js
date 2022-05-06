3f617f122c7def963e62a2ffa5c33030a98890b0
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-06 16:55:23 +0200

    Un monton de cambios.

---
+++ tech-js-estructurada/4-split/4-split.js
@@ -0,0 +1,48 @@
+const { Console } = require("console-mpds");
+const console = new Console();
+//// --------------------------------------------/
+/// <Autor: @Alberto Antelo. Autor principal. >
+/// <Autor:Manuel Rosendo Castro Iglesias.> 
+/// <Titulo: ### 4-split>
+//// --------------------------------------------/
+
+let intMin;
+let intMax;
+let intervals;
+const error = `Error!!! El máximo debe ser superior o igual al minimo`;
+let ok;
+
+do {
+    ok = true;
+    intMin = console.readNumber('Introduce el mínimo del intervalo: ');
+    intMax = console.readNumber('Introduce el maximo del intervalo (superior o igual al mínimo): ');
+    if (intMin > intMax) {
+        console.writeln(error);
+        ok = false;
+    }
+} while (!ok);
+
+do { //Está en los requerimientos. (MRCI)
+    ok = true;     //redundante, por si se modifica el código y por facilitar la lectura.
+    intervals = console.readNumber('Introduce una cantidad positiva de intervalos: ');
+    if (intervals <= 0) {
+        console.writeln(`Error!!! La cantidad debe ser positiva`);
+        ok = false;
+    }
+} while (!ok);
+
+const LenghtIntervals = (intMax - intMin) / intervals;
+let msg = `El intervalo [${intMin}, ${intMax}] dividido en ${intervals} intervalos son`;
+let init = intMin;
+for (i = 1; i <= intervals; i++) {
+    msg += ` [${init}, ${init + LenghtIntervals} ]`;
+    init += LenghtIntervals;
+    separation = ",";
+    if (i === intervals) {
+        separation = "";
+    } else if (i === intervals - 1) {
+        separation = " y";
+    }
+    msg += separation;
+}
+console.writeln(msg);
